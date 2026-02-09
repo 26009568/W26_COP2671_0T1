414b6d09e72a1770ae6eb92ceb642581f5f62bb5
Christopher Stewart <chris.stewart@go.sfcollege.edu>
2026-02-08 23:25:41 -0500

    3.3 Christopher Stewart 26009568

---
+++ Units/Sound and Effects/Prototype_3/Assets/Scripts 1/PlayerController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody playerRb;
+    public float jumpForce = 10f;
+    public float gravityModifier = 1.5f;
+    public bool isOnGround = true;
+    public bool gameOver = false;
+
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
+        {
+            playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            isOnGround = false;
+        }
+    }
+
+    // Ground = normal collider
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+    }
+
+    // Obstacle = trigger
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over");
+        }
+    }
+}
